52b7f90b62db9fa1b5dfc28f8596474b42f07667
songyang <475511896@qq.com>
2019-04-11 09:24:15 +0800

    ddd

---
+++ Azure/IConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-04-11
+ * Time: 9:17
+ */
+
+namespace Azure;
+
+
+interface IConfig
+{
+    public function get_view_config(): array ;
+}
