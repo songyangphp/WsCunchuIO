ae42a575871e26a7ed5c6f44895251cf488d1823
songyang <475511896@qq.com>
2019-04-13 09:00:31 +0800

    ddd

--- storage/StorageApp.php
+++ storage/StorageApp.php
@@ -2,25 +2,12 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2019-04-11
- * Time: 9:17
+ * Date: 2019-04-13
+ * Time: 8:59
  */
 
 namespace storage;
 
-
-interface IStorageConfig
-{
-    public function get_StorageList(): array ;
-
-    public function get_AccountName(): string ;
-
-    public function get_AccountKey() : string ;
-
-    public function get_StoreName() : string ;
-
-    public function get_Rongqi() : string ;
-}
 
 class StorageApp
 {
